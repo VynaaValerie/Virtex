c9d7dc055975ccbe170f5e828ab82ccc4813c1eb
Vynaa <118973846+VynaaValerie@users.noreply.github.com>
2024-04-19 11:24:34 +0700

    Create IPA.js

---
+++ IPA.js
@@ -0,0 +1,23 @@
+[
+    {
+        "soal": "Hewan yang mengalami metamorfosis sempurna adalah ….",
+        "jawaban": "Nyamuk dan lebah",
+        "a": "Nyamuk dan lebah",
+        "b": "Lebah dan belalang",
+        "c": "Jangkrik dan kecoa",
+        "tingkat": "Sekolah Dasar",
+        "type": "Text",
+        "link": ""
+    },
+    {
+        "soal": "Perhatikan tabel berikut!\n\n1. Cumi-cumi > Memancarkan cahaya\n2. Trenggiling > Mengeluarkan bau menyengat\n3. Ikan Lele > Memiliki misai\n4. Burung Pelatuk > Paruh runcing dan melengkung\n\nPasangan nama hewan dan cara adaptasi yang benar ditunjukkan nomor ….",
+        "jawaban": "1 dan 3",
+        "a": "1 dan 4",
+        "b": "1 dan 3",
+        "c": "2 dan 3",
+        "tingkat": "Sekolah Dasar",
+        "type": "Text",
+        "link": ""
+    },
+    // Masukkan pertanyaan-pertanyaan IPA lainnya di sini...
+]
